cda5ef655008ad093282d6be3938a4085bf6d9b7
Winstonshih <Winstonshih@github.com>
2026-08-25 17:52:59 +0000

    Test run: 6/6 passed, 0 failed, 0 skipped - 2026-08-25T17:52:59.126Z

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 
 public class Isogram {
     /**
@@ -13,6 +14,16 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        for(int i=0;i<str.length();i++)
+        {
+            for(int j=i+1;j<str.length();j++)
+            {
+                if(str.charAt(i)==str.charAt(j))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
